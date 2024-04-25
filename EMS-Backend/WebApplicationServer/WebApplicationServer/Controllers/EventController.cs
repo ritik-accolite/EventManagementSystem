9469c26e30fda0558c176be7c54a622c11012c5f
Ritik Kumar <Ritik.K_7767@accolite.com>
2024-04-25 15:49:53 +0530

    Added Test APIs for Users

---
+++ EMS-Backend/WebApplicationServer/WebApplicationServer/Controllers/EventController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+    }
+}
--- EMS-Backend/WebApplicationServer/WebApplicationServer/Program.cs
+++ EMS-Backend/WebApplicationServer/WebApplicationServer/Program.cs
@@ -1,9 +1,16 @@
+
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
 
 // Register CORS services
 builder.Services.AddCors(options =>
@@ -12,10 +19,18 @@ builder.Services.AddCors(options =>
         builder =>
         {
             builder.AllowAnyOrigin();
-            //builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+            //builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
         });
 });
 
+// Create a SqlConnection object using the connection string
+using var conn = new SqlConnection(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING"));
+conn.Open(); // Open the connection
+// Configure DbContextOptionsBuilder to use the SqlConnection
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    options.UseSqlServer(conn);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -46,7 +61,12 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
-
+// Uncomment this for access through 1 origin only
+//app.UseCors(AllowOrigin)
+app.UseRouting();
+app.MapControllerRoute(
+    name: default,
+    pattern: "{controller=Person}/{action = GetPerson}");
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
