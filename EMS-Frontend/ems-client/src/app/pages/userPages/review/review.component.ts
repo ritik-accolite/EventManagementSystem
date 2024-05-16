985067cf14cf2a36ebef91821531f0fd7655cf12
pratibhamishra11 <pratibha.mishra@accolitedigital.com>
2024-05-16 10:02:32 -0700

    review

--- EMS-Frontend/ems-client/src/app/pages/userPages/review/review.component.ts
+++ EMS-Frontend/ems-client/src/app/pages/userPages/review/review.component.ts
@@ -1,23 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserdataService } from '../../../services/userDataService/userdata.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { JwtDecodeService } from '../../../services/jwtDecodeService/jwtDecode.service';
 
 @Component({
   selector: 'app-review',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './review.component.html',
   styleUrl: './review.component.css'
 })
-export class ReviewComponent {
+export class ReviewComponent implements OnInit{
   reviewForm: FormGroup
   eventId: number = 0;
+  id: string = '';
+  status: number = 0;
 
   constructor(private fb: FormBuilder , private userdataservice: UserdataService , private route: ActivatedRoute,
-    private router: Router) {
+    private router: Router , private jwtDecodeService: JwtDecodeService) {
     this.reviewForm = this.fb.group({
-      
+      Description: ['', Validators.required],
+      Rating: ['', [Validators.required, Validators.min(1), Validators.max(5)]],
+      IsReported: [false]
     });
+  }
+
+  ngOnInit(): void {
+      this.id = this.jwtDecodeService.id;
+      this.route.params.subscribe(params => {
+        this.eventId= this.userdataservice.eventId;
+      });
+  }
+
+  onSubmit() {
+    if (this.reviewForm.valid) {
+      const formData = {
+        ...this.reviewForm.value,
+        EventId: this.eventId,
+        UserId: this.id
+      };
+
+      this.userdataservice.review(formData).subscribe(
+        (response) => {
+          this.status = response.status;
+          if (this.status === 200) {
+            console.log('Review added successfully : ',response.message);
+          }
+        },
+        (error) => {
+          console.error('Error adding event',error);
+        }
+      );
+    }
   }
 }
